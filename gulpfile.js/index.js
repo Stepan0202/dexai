9b95c1f364354d67b5989436d7eb7b4763529fb9
Stepan0202 <stepan020293@gmail.com>
2023-08-21 01:33:45 +0300

    Gulp

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,19 +1,47 @@
 'use strict'
-const {src, dest} = require('gulp');
+const {src, dest, series, parallel, watch} = require('gulp');
 const fileInclude = require('gulp-file-include');
+const gulpSass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
 
 
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-}
+
 
 function htmlInclude(){
-    return src('./src/**/*.html')
+    return src('./src/*.html')
     .pipe(fileInclude())
     .pipe(dest('./dist/'));
 }
 
+function scssCompiler(){
+    return src('./src/**/*.scss')
+    .pipe(gulpSass())
+    .pipe(dest('./dist/css/'))
+}
+function browserSyn(){
+    browserSync.init({
+        server: { 
+            baseDir: "./dist/"
+        }
+    });
+}
+function browserReload(cb){
+    browserSync.reload();
+    cb();
+}
 
-  exports.default = defaultTask;
+function watchTask(){
+    watch('./src/**/*.html', series(htmlInclude, browserReload)),
+    watch('./src/**/*.scss', series(scssCompiler, browserReload))
+}
+
+  exports.default = series(
+    htmlInclude,
+    scssCompiler,
+    parallel(browserSyn, watchTask)
+)
+    
   exports.fileInclude = htmlInclude;
+  exports.scssCompiler = scssCompiler;
+  exports.browserSync = browserSyn;
+  exports.watchTask = watchTask;
